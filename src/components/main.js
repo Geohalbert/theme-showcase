14d4ee05928892249d084c73635a2de7378d4bfe
Hal Peters <geohalbert@gmail.com>
2019-11-17 17:00:32 -0600

    main component loads views

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import ListView from "../../views/ListView.js";
+import QuakeView from "../../views/QuakeView.js";
 
 function Main() {
   return (
     <main>
-      <h2>Main component, use react hooks for theme switches?</h2>
+      <Switch>
+        <Route exact path="/" component={ListView} />
+        <Route path="/quake/quake:id" component={QuakeView} />
+      </Switch>
     </main>
   );
 }
