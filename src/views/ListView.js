bed96708d0b90be3aacff23f13d59cdfab103b1e
Hal Peters <geohalbert@gmail.com>
2019-11-18 00:15:03 -0600

    updated listview

--- src/views/ListView.js
+++ src/views/ListView.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import QuakeService from "../services/QuakeService";
 
 class ListView extends React.Component {
   constructor() {
@@ -6,14 +8,31 @@ class ListView extends React.Component {
     this.state = {
       quakes: []
     };
+
+    this.quakeService = new QuakeService();
   }
 
-  componenDidMount() {}
+  componenDidMount() {
+    this.quakeService.getQuakeList.then(response => {
+      console.log("response: ", response);
+      this.setState({ quakes: response.features });
+    });
+  }
+
+  renderQuakes = () => {
+    return this.state.quakes.map((quake, key) => {
+      return (
+        <li key={key}>
+          <Link to={`/quake/${quake.id}`}>Quake {quake.id}</Link>
+        </li>
+      );
+    });
+  };
 
   render() {
     return (
       <div>
-        <h3>Quake List:</h3>
+        <ul>{this.renderQuakes()}</ul>
       </div>
     );
   }
