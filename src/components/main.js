a6dc8bd771b73e9e05d3891f07148415e1547707
Hal Peters <geohalbert@gmail.com>
2019-11-21 11:23:51 -0600

    adding query inputs\

--- src/components/main.js
+++ src/components/main.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import ListView from "../views/ListView.js";
 import QuakeView from "../views/QuakeView.js";
+import QueryForm from "../views/QueryForm.js";
 
 function Main() {
   return (
     <main>
       <Switch>
-        <Route exact path="/" component={ListView} />
-        <Route path="/quake/quake:id" component={QuakeView} />
+        <Route exact path="/" component={QueryForm} />
+        <Route exact path="/quakes" component={ListView} />
+        <Route path="/quakes/quake:id" component={QuakeView} />
       </Switch>
     </main>
   );
