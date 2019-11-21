7a7bbd5acaf9fddcd91e8e711c2d29dc9d010e40
Hal Peters <geohalbert@gmail.com>
2019-11-21 11:51:13 -0600

    updated quake services

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import ListView from "../views/ListView.js";
 import QuakeView from "../views/QuakeView.js";
 import QueryForm from "../views/QueryForm.js";
 
@@ -9,7 +8,6 @@ function Main() {
     <main>
       <Switch>
         <Route exact path="/" component={QueryForm} />
-        <Route exact path="/quakes" component={ListView} />
         <Route path="/quakes/quake:id" component={QuakeView} />
       </Switch>
     </main>
--- src/services/QuakeService.js
+++ src/services/QuakeService.js
@@ -2,25 +2,9 @@ import axios from "axios";
 const API_URL =
   "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
 
-function dates(date) {
-  const d = new Date();
-  let month = `${d.getMonth() + 1}`;
-  let today = `${d.getDate()}`;
-  let yesterday = `${d.getDate() - 1}`;
-  let day;
-  date === "yesterday" ? (day = yesterday) : (day = today);
-  const year = d.getFullYear();
-
-  if (month.length < 2) month = `0${month}`;
-  if (day.length < 2) day = `0${day}`;
-
-  return [year, month, day].join("-");
-}
 class QuakeService {
-  async getQuakeList() {
-    const url = `${API_URL}&starttime=${dates("yesterday")}&endtime=${dates(
-      "today"
-    )}&limit=10`;
+  async getQuakeList(queryString) {
+    const url = `${API_URL}${queryString}`;
     return axios.get(url).then(response => response.data.features);
   }
 
