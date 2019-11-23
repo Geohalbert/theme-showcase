d95960fa8de24ef8a3f1a5a6fcca464e94aac4e5
Hal Peters <geohalbert@gmail.com>
2019-11-23 05:54:22 -0600

    having issues grabbing coordinate data

--- src/views/QuakeView.js
+++ src/views/QuakeView.js
@@ -6,7 +6,8 @@ class QuakeView extends React.Component {
     super(props);
     this.state = {
       quakeId: "",
-      properties: {}
+      properties: {},
+      coordinates: []
     };
 
     this.quakeService = new QuakeService();
@@ -16,17 +17,22 @@ class QuakeView extends React.Component {
     const quakeId = this.props.match.params.id;
     this.quakeService.getQuake(quakeId).then(response => {
       console.log("quake properties: ", response.properties);
-      this.setState({ quakeId: response.id, properties: response.properties });
+      this.setState({
+        quakeId: response.id,
+        properties: response.properties,
+        coordinates: response.geometry.coor
+      });
     });
   }
 
   render() {
-    const { quakeId, properties } = this.state;
+    const { quakeId, properties, coordinates } = this.state;
     return (
       <div>
         <div className="viewQuake">Quake {quakeId}</div>
         <div className="quake-properties">
           <div>Place: {properties.place}</div>
+          <div>Coordinates: {coordinates}</div>
           <div>Magnitude: {properties.mag}</div>
           <div>Time: {properties.time}</div>
         </div>
