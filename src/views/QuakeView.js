36468658d533e4cd075dd9f2c2987fa0d4303327
Hal Peters <geohalbert@gmail.com>
2019-11-26 23:24:18 -0600

    working on implementing map

--- src/views/QuakeView.js
+++ src/views/QuakeView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import QuakeService from "../services/QuakeService";
+import "../themes/css/QueryForm.css";
 
 class QuakeView extends React.Component {
   constructor(props) {
@@ -29,14 +30,15 @@ class QuakeView extends React.Component {
     const convertTime = new Date(properties.time).toLocaleString();
     const coors = coordinates.join(", ");
     return (
-      <div>
-        <div className="viewQuake">Quake Id: {quakeId}</div>
+      <div id="quake">
         <div className="quake-details">
+          <div className="quake-property">Quake Id: {quakeId}</div>
           <div className="quake-property">Place: {properties.place}</div>
           <div className="quake-property">Coordinates: {coors}</div>
           <div className="quake-property">Magnitude: {properties.mag}</div>
           <div className="quake-property">Time: {convertTime}</div>
         </div>
+        {/* {coordinates && <GoogleMap coordinates={coordinates} />} */}
       </div>
     );
   }
